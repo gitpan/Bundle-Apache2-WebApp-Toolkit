a4c43c6c226d15d7d30d3031828dcce1bd8d860c
Marc S. Brooks <mbrooks@cpan.org>
2011-01-08 13:22:08 +0000

    Import of MBROOKS/Bundle-Apache2-WebApp-Toolkit-0.09 from CPAN.
    gitpan-cpan-distribution: Bundle-Apache2-WebApp-Toolkit
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         MBROOKS/Bundle-Apache2-WebApp-Toolkit-0.09.tar.gz
    gitpan-cpan-author:       MBROOKS
    gitpan-cpan-maturity:     released

--- lib/Bundle/Apache2/WebApp/Toolkit.pm
+++ lib/Bundle/Apache2/WebApp/Toolkit.pm
@@ -12,7 +12,7 @@
 
 package Bundle::Apache2::WebApp::Toolkit;
 
-our $VERSION = 0.08;
+our $VERSION = 0.09;
 
 1;
 
@@ -80,7 +80,7 @@ Marc S. Brooks, E<lt>mbrooks@cpan.orgE<gt> - L<http://mbrooks.info>
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
-See L<http://www.perl.com/perl/misc/Artistic.html>
+See L<http://dev.perl.org/licenses/artistic.html>
 
 =cut
 
